2cefdc0c942b486bac3ba2925594fdd694e0f887
pranay <pranaygavhale12@gmail.com>
2020-04-18 15:36:24 +0530

    this is add-07

--- addition.py
+++ addition.py
@@ -1,7 +1,6 @@
-def add(a,b):
-    return(a+b)
-
-x=float(input("enter first number:"))
-y=float(input("enter second number:"))
-c=add(x,y)
-print("addition of",x,"and",y,"is:",c)
+class Calculate():
+    def addition(self):
+        x=self.first_no=float(input("enter first number:"))
+        y=self.second_no=float(input("enter second number:"))
+        z=x+y
+        print("addition of",x,"and",y,"is:",z)
