494187e0168f2bcdf40d7c1edc272aa9fecda118
pranay <pranaygavhale12@gmail.com>
2020-04-18 15:34:19 +0530

    this is add-05

--- addition.py
+++ addition.py
@@ -1,4 +1,7 @@
-a=float(input('enter first number'))
-b=float(input('enter second number'))
-c=a+b
-print("sum of {0} and {1} is {2}".format(a,b,c))
+def add(a,b):
+    c=a+b
+    print('addition of',a,"and",b,"is:",c)
+
+x=float(input("enter first number:"))
+y=float(input("enter second number:"))
+add(x,y)
